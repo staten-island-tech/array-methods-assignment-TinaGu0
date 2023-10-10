790ae99ffd6a312820a281050c5e3d2f7cacc6c1
TinaGu0 <tinaguo0904@gmail.com>
2023-10-10 10:20:36 -0400

    weeeeee

---
+++ app.js
@@ -0,0 +1,24 @@
+const petra = {
+    firstname : Petra,
+    lastname : Gurin,
+    height : 152,
+    groupleader : false,
+}
+
+const selen = {
+    firstname : Selen, 
+    lastname : Tatsuki,
+    height : 160,
+    groupleader : false
+}
+
+const rosemi = {
+    firstname : Rosemi,
+    lastname : Lovelock,
+    height : 150,
+    groupleader : true
+}
+
+const obsydia = [petra , selen , rosemi]
+
+console.log(obsydia)
