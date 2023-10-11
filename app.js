8e89057ddf4c9721d993e94c87bd908422803c13
TinaGu0 <tinaguo0904@gmail.com>
2023-10-11 10:13:32 -0400

    eeeeeeeeee

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const petra = {
-    firstname : Petra,
-    lastname : Gurin,
-    height : 152,
-    groupleader : false,
-}
-
-const selen = {
-    firstname : Selen, 
-    lastname : Tatsuki,
-    height : 160,
-    groupleader : false
-}
-
-const rosemi = {
-    firstname : Rosemi,
-    lastname : Lovelock,
-    height : 150,
-    groupleader : true
-}
-
-const obsydia = [petra , selen , rosemi]
-
+const obsydiaArr = [
+    {
+        firstname : Petra,
+        lastname : Gurin,
+        height : 152,
+        groupleader : false,
+    } ,
+    {
+        firstname : Selen, 
+        lastname : Tatsuki,
+        height : 160,
+        groupleader : false
+    } ,
+    rosemi = {
+        firstname : Rosemi,
+        lastname : Lovelock,
+        height : 150,
+        groupleader : true
+    } 
+]
 console.log(obsydia)
