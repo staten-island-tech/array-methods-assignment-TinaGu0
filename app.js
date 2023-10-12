053c03e8f147e7c27bb892338cbda996fd6bce4e
TinaGu0 <tinaguo0904@gmail.com>
2023-10-12 10:03:08 -0400

    final

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const obsydia = [
-    {
-        firstname : "Petra",
-        lastname : "Gurin",
-        height : 152,
-        groupmembers : ["Selen" , "Rosemi"]
-    } ,
-    {
-        firstname : "Selen", 
-        lastname : "Tatsuki",
-        height : 160,
-        groupmembers : ["Petra" , "Rosemi"]
-    } ,
-    {
-        firstname : "Rosemi",
-        lastname : "Lovelock",
-        height : 150,
-        groupmembers : ["Petra" , "Selen"]
-    } 
-];
-
-obsydia.forEach(function(obsydia) {
-    console.log(obsydia.firstname)
-}
-);
-
-obsydia.forEach(function(obsydia) {
-    console.log(obsydia.groupmembers)
-}
-);
+const obsydia = {
+    members: [{
+            firstname: "Petra",
+            lastname: "Gurin",
+            height: 152,
+            groupmembers: ["Selen" , "Rosemi"]
+        } ,
+        {
+            firstname: "Selen", 
+            lastname: "Tatsuki",
+            height: 160,
+            groupmembers: ["Petra" , "Rosemi"]
+        } ,
+        {
+            firstname: "Rosemi",
+            lastname: "Lovelock",
+            height: 150,
+            groupmembers: ["Petra" , "Selen"]
+        } 
+    ]    
+};
 
 
-//:D? fix this tmr in class?? 
-obsydia.filter(function(obsydia) {
-    console.log(obsydia.groupmembers)
-}
-)
+obsydia.members.forEach(function(members) {
+    console.log(members.firstname)
+});
+
+obsydia.members.forEach(function(members) {
+    members.groupmembers.forEach(function(groupmembers) {
+    console.log(groupmembers)
+    },
+    )});
+
+const obsydiafiltered = obsydia.members.filter(function(obsydia) {
+    return obsydia.height < 155
+});
+console.log(obsydiafiltered)
